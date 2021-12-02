e052b7ea5b90884d70118fe5e1680932ed990394
Bryan Fernandes <bryan.efernandes@gmail.com>
2021-12-02 16:03:44 -0300

    Challenges - Exercicio 2

--- src/challenges.js
+++ src/challenges.js
@@ -7,10 +7,10 @@ function compareTrue(p1, p2) {
   }
 }
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let resultado = (base * height) / 2;
+  return resultado;
 }
-
 // Desafio 3
 function splitSentence() {
   // seu código aqui
